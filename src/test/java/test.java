a44c54b66c08265bb77476f47e07c9a8b7e76a32
Rahul Sharma <rahul.sharmaa9001@gmail.com>
2025-07-01 16:28:25 +0530

    Minor Change Added for the FirstRunDriver

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,4 +1,5 @@
 import SelfHealing.CustomWebDriver;
+import SelfHealing.FirstRunDriver;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
